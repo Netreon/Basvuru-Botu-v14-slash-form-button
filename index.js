ad20b49190fefaf4f12b150fa3f208775d7f6c8a
Netreon <132587220+Netreon@users.noreply.github.com>
2023-06-18 13:49:51 +0300

    Olmayan Event Handler silindi

--- index.js
+++ index.js
@@ -49,19 +49,6 @@ client.on("ready", async () => {
     console.log("Durum güncellendi.");
 });
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
